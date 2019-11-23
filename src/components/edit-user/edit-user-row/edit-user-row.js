7282ea6fc5f0ab707316ffcc3d7ecbaccccdb72c
kreuzfeur <kreuzfeur@gmail.com>
2019-11-24 02:12:43 +0300

    test

--- src/components/edit-user/edit-user-row/edit-user-row.js
+++ src/components/edit-user/edit-user-row/edit-user-row.js
@@ -48,7 +48,7 @@ export default class EditUserRow extends Component {
             }
          })
     }
-    //test jenki
+    //test jenk
     isDisabled = () => (this.props.username === 'admin' ? true : false);
     enableSave = () => this.setState({ onSaveDisabled: false })
     render() {
