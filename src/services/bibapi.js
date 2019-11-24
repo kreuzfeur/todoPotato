75ecd613f836bc95ef515b6a7e1238e95511cc19
kreuzfeur <kreuzfeur@gmail.com>
2019-11-24 17:48:48 +0300

    asd

--- src/services/bibapi.js
+++ src/services/bibapi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://31.132.169.72//potatoTodoApi/public/api/';
+axios.defaults.baseURL = 'http://31.132.169.72/potatoTodoApi/public/api/';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
